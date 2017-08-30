f5dbf0b6b6ee9d50cb138441f966051f789fdf5a
Jerome Gravel-Niquet <jeromegn@gmail.com>
2017-08-30 13:01:00 -0400

    add header and redirect test

--- functions/req/index.js
+++ functions/req/index.js
@@ -1,6 +1,19 @@
 console.log('starting function')
 exports.handle = function(e, ctx, cb) {
   console.log('processing event: %j', e)
+  if (e.request.headers["x-test"] == "testing") {
+    return cb(null, {
+      response: {
+        status_code: 302,
+        cookies: [
+          {name: "hellocookie", value: "hola"}
+        ],
+        headers: {
+          "location": "http://example.com"
+        }
+      }
+    })
+  }
   cb(null, {
     request: {
       headers: {
